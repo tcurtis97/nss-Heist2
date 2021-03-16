b9743ca9c261b9df020e246785138c35d431076b
Tim Curtis <timcurtis97@yahoo.com>
2021-03-16 14:17:49 -0500

    finished Hacker class

--- Muscle.cs
+++ Muscle.cs
@@ -4,6 +4,17 @@ namespace Heist2
 {
     public class Muscle : IRobber
     {
-
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public int PercentageCut { get; set; }
+        public void PerformSkill(int Bank)
+        {
+            int beatenGuard = Bank - SkillLevel;
+            Console.WriteLine($"{Name} is hacking the alarm system. Decreased security {SkillLevel} points");
+            if (hackedAlarm <= 0)
+            {
+                Console.WriteLine($"{Name} has disabled the alarm system!");
+            }
+        }
     }
 }
