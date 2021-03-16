4714fe831fa675947951832aa0867c766eebf195
Tim Curtis <timcurtis97@yahoo.com>
2021-03-16 14:21:21 -0500

    finished LockSpecialist class

--- LockSpecialist.cs
+++ LockSpecialist.cs
@@ -4,6 +4,17 @@ namespace Heist2
 {
     public class LockSpecialist : IRobber
     {
-
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public int PercentageCut { get; set; }
+        public void PerformSkill(int Bank)
+        {
+            int pickedVault = Bank - SkillLevel;
+            Console.WriteLine($"{Name} is cracking the vault. Decreased security {SkillLevel} points");
+            if (pickedVault <= 0)
+            {
+                Console.WriteLine($"{Name} has cracked open the vault!");
+            }
+        }
     }
 }
